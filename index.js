70d61e2b45aa9633cb39e5830ad91fda64c3d3e2
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-13 21:18:57 +0800

    added show details modal btn

--- index.js
+++ index.js
@@ -18,7 +18,32 @@ const loadAllPhones = async(status, searchText) => {
 }
 
 const displayAllPhones = (phones) => {
-  console.log(phones);
+  const phonesContainer = document.getElementById('phones-container');
+
+  phones.forEach(phone => {
+    const {brand, phone_name, slug, image} = phone;
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card bg-base-100 w-96 mb-2 shadow-sm">
+  <figure class="px-10 pt-10">
+    <img
+      src=${image}
+      alt="Shoes"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">${brand}</h2>
+    <p>${slug}</p>
+    <div class="card-actions">
+      
+      <button onclick="phoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
+    </div>
+  </div>
+</div>
+    `
+
+    phonesContainer.appendChild(div)
+  })
 }
 
 
@@ -35,5 +60,34 @@ const handleSearch = () => {
 const handleShowAll = () => {
   loadAllPhones(true)
 }
+
+
+const phoneDetails = async(slugs) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${slugs}`)
+  const data = await res.json();
+  console.log(data.data)
+
+  const {brand, image, slug} = data.data
+
+  const modalContainer = document.getElementById('modal-container');
+
+  modalContainer.innerHTML = `
+  <dialog id="my_modal_1" class="modal">
+        <div class="modal-box flex flex-col items-center">
+          <h3 class="text-lg font-bold">${brand}</h3>
+          <img src='${image}'/>
+          <p class="py-4">Press ESC key or click the button below to close</p>
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+  `
+  my_modal_1.showModal();
+}
+
 
 loadAllPhones(false, "iphone");
